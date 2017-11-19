f25e04977f3559b99a38ded1c19f3b4b9d3460ef
Matheus Miranda <matheusmirandalacerda@gmail.com>
2017-11-19 13:27:42 -0200

    Remove useless tokens and simplify parser
    Signed-off-by: Guilherme Lacerda <guiglacerda@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- parser_lispf_ck.py
+++ parser_lispf_ck.py
@@ -1,56 +1,18 @@
 import ox
 
 lexer = ox.make_lexer([
-    ('NAME',r'^(?!;|def|loop|doBefore|doAfter|sub|dec|add|inc|print)[a-zA-Z]+'),
+    ('NAME',r'[a-zA-Z]+'),
     ('NUMBER', r'\d+'),
-    ('FUNCTION_DECLARATION',r'def'),
-    ('EXECUTION_BLOCK',r'do'),
-    ('READ_INPUT',r'read'),
-    ('PRINT_OUTPUT',r'print'),
-    ('INCREMENT_OPERATOR',r'inc'),
-    ('DECREMENT_OPERATOR',r'dec'),
-    ('OPENING_BRACKET',r'\('),
-    ('CLOSING_BRACKET',r'\)'),
-    ('DO_BEFORE_OPERATOR',r'doBefore'),
-    ('DO_AFTER_OPERATOR',r'doAfter'),
-    ('ADD_OPERATOR',r'add'),
-    ('SUB_OPERATOR',r'sub'),
-    ('LOOP_OPERATOR', r'loop'),
 ])
 
-tokens_list = ['NUMBER', 'FUNCTION_DECLARATION', 'EXECUTION_BLOCK', 'READ_INPUT',
-     'PRINT_OUTPUT', 'INCREMENT_OPERATOR', 'DECREMENT_OPERATOR',
-     'OPENING_BRACKET', 'CLOSING_BRACKET', 'DO_BEFORE_OPERATOR', 'DO_AFTER_OPERATOR',
-     'ADD_OPERATOR','SUB_OPERATOR','NAME', 'LOOP_OPERATOR']
+tokens_list = ['NUMBER', 'NAME']
 
 atom_number = lambda value: ('atom_number',float(value))
-atom_do = lambda value: ('atom_do','do')
-simple_operation = lambda operator : ('simple_operation',operator)
-read_operation = lambda read_operator : ('read_operation',read_operator)
-print_operation = lambda print_operator : ('print_operation',print_operator)
-add_operation = lambda add_operator, value : ('add_operation',(add_operator,value))
-sub_operation = lambda sub_operator, value : ('sub_operation',(sub_operator,value))
-do_before_block = lambda do_before_key, block : ('do_before_block',(do_before_key,block))
-do_after_block = lambda do_after_key, block : ('do_after_block',(do_after_key,block))
-function_declaration = lambda do, function_name, block: ('function_declaration', do, function_name, block)
-loop_block_operator = lambda loop, block : ('loop_structure', loop, block)
 
 parser = ox.make_parser([
-    ('block : FUNCTION_DECLARATION atom block', function_declaration),
-    ('block : simple_block', lambda block: block),
-    ('simple_block : DO_AFTER_OPERATOR simple_block',do_after_block),
-    ('simple_block : DO_BEFORE_OPERATOR simple_block',do_before_block),
-    ('simple_block : LOOP_OPERATOR simple_block', loop_block_operator),
     ('simple_block : simple_term', lambda simple_block: simple_block),
     ('simple_term : simple_term atom',lambda first_term, second_term : (first_term, second_term)),
     ('simple_term : atom', lambda term: term),
-    ('atom : SUB_OPERATOR NUMBER', sub_operation),
-    ('atom : ADD_OPERATOR NUMBER', add_operation),
-    ('atom : PRINT_OUTPUT', print_operation),
-    ('atom : READ_INPUT', read_operation),
-    ('atom : INCREMENT_OPERATOR', simple_operation),
-    ('atom : DECREMENT_OPERATOR', simple_operation),
-    ('atom : EXECUTION_BLOCK', atom_do),
     ('atom : NUMBER', atom_number),
     ('atom : NAME',lambda name : name),
 ], tokens_list)
