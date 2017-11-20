8f6e5a99318cff7273aab39e92de23bdbd527bc1
Matheus Miranda <matheusmirandalacerda@gmail.com>
2017-11-20 09:25:33 -0200

    Add logic to use commas on function's arguments
    Signed-off-by: Guilherme Lacerda <guiglacerda@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- parser_lispf_ck.py
+++ parser_lispf_ck.py
@@ -5,9 +5,10 @@ lexer = ox.make_lexer([
     ('NUMBER', r'\d+'),
     ('OPENING_PARENTHESES', r'\('),
     ('CLOSING_PARENTHESES', r'\)'),
+    ('COMMA',r'\,'),
 ])
 
-tokens_list = ['NUMBER', 'NAME','OPENING_PARENTHESES','CLOSING_PARENTHESES']
+tokens_list = ['NUMBER', 'NAME','OPENING_PARENTHESES','CLOSING_PARENTHESES','COMMA']
 
 atom_number = lambda value: ('atom_number',float(value))
 
@@ -16,6 +17,7 @@ parser = ox.make_parser([
     ('simple_block : simple_term', lambda simple_block: simple_block),
     ('simple_term : OPENING_PARENTHESES simple_term CLOSING_PARENTHESES', lambda opening_paretheses, term, closing_parentheses: (opening_paretheses, term, closing_parentheses)),
     ('simple_term : atom simple_term',lambda first_term, second_term : (first_term, second_term)),
+    ('simple_term : atom COMMA simple_term',lambda atom, comma, simple_term : (atom, comma, simple_term)),
     ('simple_term : atom', lambda term: term),
     ('atom : OPENING_PARENTHESES atom CLOSING_PARENTHESES', lambda opening_paretheses, term, closing_parentheses: (opening_paretheses, term, closing_parentheses)),
     ('atom : NUMBER', atom_number),
